9807763a0b3db5b4380b596112c124834f3ec38b
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-02-10 20:19:35 +0300

    new

---
+++ main.js
@@ -0,0 +1,25 @@
+let time = document.querySelector("#time");
+let date = document.querySelector("#date");
+
+setInterval(()=>{
+today();
+},1000);
+
+
+function today() {
+
+let today = new Date();
+
+const monthNames =["jan","feb","Mar","Apr","May","June","Jul","Aug","Sep","Nov","Dec"];
+
+const dayNames=["Sat","Sun","Mon","Tues","Wed","Thur","Fri"];
+
+let currentTime = `${today.getHours()  < 9 ? '0' + today.getHours() : today.getHours()} : ${today.getMinutes() < 9 ? '0' + today.getMinutes() : today.getMinutes() } : ${today.getSeconds() < 9 ? '0' + today.getSeconds(): today.getSeconds()}`;
+
+let currentDate = `${dayNames[today.getDay()+1]}, ${today.getDate()} ${monthNames[today.getMonth()]},${today.getFullYear()}`;
+
+time.innerHTML= currentTime;
+
+date.innerHTML = currentDate;
+
+}
